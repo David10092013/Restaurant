d82c20a83ab84b42a018d907e6f46f0e5da3a115
David Fortunato <david.fortunato05@gmail.com>
2022-03-19 09:54:28 +0200

    19/03/2022

--- html/js/index.js
+++ html/js/index.js
@@ -2,3 +2,16 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.toggle('active');
 };
+
+let market = document.querySelector('.navbar');
+document.querySelector('#market-btn').onclick = () => {
+  market.classList.toggle('active');
+};
+
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('scrolling-active', windowPosition);
+});
+
+
